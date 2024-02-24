06fa297d0b0f982567be6ddce12e83c417f0f5a5
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2024-02-25 00:52:59 +0100

    update

--- app/src/main.c
+++ app/src/main.c
@@ -8,10 +8,69 @@
 #include <app_version.h>
 
 #include <zephyr/logging/log.h>
+
+#include "flecs.h"
+
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+
+void my_abort() {
+	printk("myabort\n");
+	abort();
+}
+
+
+void my_logger(int32_t level, const char *file, int32_t line, const char *msg) {
+	printk("level:%i file:%s:%i msg:%s\n", level, file, line, msg);
+	if (level < -3) {
+		my_abort();
+	}
+}
+
+
+static int total_size = 0;
+
+static void* my_malloc(ecs_size_t size) {
+	void * p = malloc((size_t)size);
+	total_size += size;
+	printk("malloc: sum:%10i %10i, %p,\n", total_size, size, p);
+	return p;
+}
+
+static void* my_calloc(ecs_size_t size) {
+	void * p = calloc(1, (size_t)size);
+	total_size += size;
+	printk("calloc: sum:%10i %10i, %p\n", total_size, size, p);
+	return p;
+}
+
+static void my_free(void * p) {
+	printk("free: %x\n", p);
+	free(p);
+}
+
+/*
+void on_heap_free(uintptr_t heap_id, void *mem, size_t bytes)
+{
+  LOG_INF("Memory freed at %p, size %ld", mem, bytes);
+}
+
+HEAP_LISTENER_FREE_DEFINE(my_listener, HEAP_ID_LIBC, on_heap_free);
+*/
 
 int main(void)
 {
+    ecs_os_set_api_defaults();
+    ecs_os_api_t os_api = ecs_os_api;
+    os_api.log_ = my_logger;
+    os_api.abort_ = my_abort;
+    os_api.malloc_ = my_malloc;
+    os_api.calloc_ = my_calloc;
+    os_api.free_ = my_free;
+    ecs_os_set_api(&os_api);
+
+	printk("ecs_mini\n");
+	ecs_world_t * world = ecs_mini();
+
 	int ret;
 	const struct device *sensor;
 
